2dfa0722dcdc42adf14bd15776d29234fb3970df
MarilaMorales <mmorales@fwdcostarica.com>
2024-09-06 08:13:07 -0600

    Carpetas

---
+++ practicaFinalREact/src/Routes/Routing.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from "../Pages/Login.jsx"
+import Registro from "../Pages/Registro.jsx"
+import Tareas from '../Componentes/Tareas.jsx';
+
+
+const Routing = () => {
+  return (
+    <Router>
+     <Routes>
+       {/* <Route path="/Login" element={<Login />} />
+       <Route path="/Registro" element={<Registro />} /> */}
+        <Route path="/Tareas" element={<Tareas />} />
+     </Routes>
+    </Router>
+   );
+};
+export default Routing;
