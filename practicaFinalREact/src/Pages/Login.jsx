a28a76973bcc3f08d8ec79b58b5227c53d09e953
MarilaMorales <mmorales@fwdcostarica.com>
2024-09-06 09:19:26 -0600

    LoginForm

---
+++ practicaFinalREact/src/Pages/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Registro.css'
+import Tareas from '../Componentes/Tareas'
+
+function Registro() {
+  return (
+    <div>
+      <Tareas />
+    </div>
+  )
+}
+
+export default Registro
